34c4eb06771952aaf9315d4e9dd6696d477f3931
guigui55 <guigui55@outlook.jp>
2024-08-16 12:30:08 +0900

    240816-1229

---
+++ django_test/django_test1.py
@@ -0,0 +1,43 @@
+
+
+"""
+
+https://qiita.com/pythonista/items/19613663ef7bb3c57d4f
+ pip install django
+
+ djangoプロジェクトの作成
+ django-admin startproject test_project1
+
+一階層目の ディレクトリに移動して以降の各種設定を行います。
+cd test_project1
+
+
+ プロジェクト設定
+config/settings.py 変更
+
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
+
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
+
+
+
+migrate Postgre
+https://qiita.com/honda28/items/d5bf743c3c244791f559
+
+
+https://qiita.com/prg_mt/items/c5c8edbf45dfe87b54ef
+
+
+
+DATABASES = {
+
+"""
+
+def _test_main():
+    pass
+
+
+if __name__ == '__main__':
+    _test_main()
